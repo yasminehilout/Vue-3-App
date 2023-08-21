16a1d786858f04f8920408498628437474ffeb30
yasminehilout <yasmine.hilout@gmail.com>
2023-08-20 21:56:59 -0400

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    //data is a function that returns an object
+    data() {
+        //return an options object
+        return {
+            product: 'Socks'
+        }
+    }
+})
